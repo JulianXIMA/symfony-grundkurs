020e65f48ea8b263851cb871b1fc5e0b3ede0f3c
Julian Fuchs <jfu@xima.de>
2021-06-09 16:11:11 +0200

    Symfony Kernkomponenten Teil 2

--- src/Controller/ArtikelController.php
+++ src/Controller/ArtikelController.php
@@ -13,18 +13,28 @@ class ArtikelController extends AbstractController
     #[Route('/artikel', name: 'artikel')]
     public function index(Request $request): Response
     {
-        /*return $this->render('artikel/index.html.twig', [
-            'controller_name' => 'ArtikelController',
-        ]);*/
-
-        $artikel = new Artikel();
-        $artikel->setTitel("Unser erster Artikel");
-
         $em = $this->getDoctrine()->getManager();
 
-        $em->persist($artikel);
+        //Daten zur DB hinzufügen
+        //$artikel = new Artikel();
+        //$artikel->setTitel("Unser erster Artikel");
+        //$em->persist($artikel);
+        //$em->flush();
+        //$getArtikel = $em->getRepository(Artikel::class)->findAll();
+
+        //Daten aus DB auslesen (wird auch für Löschen benötigt)
+        $getArtikel = $em->getRepository(Artikel::class)->findOneBy([
+            'id'=>1
+        ]);
+
+        //Daten aus DB löschen
+        $em->remove($getArtikel);
         $em->flush();
 
-        return new Response("Artikel wurde angelegt");
+        return $this->render('artikel/index.html.twig', [
+            'artikel' => $getArtikel,
+        ]);
+
+        //return new Response("Artikel wurde angelegt");
     }
 }
